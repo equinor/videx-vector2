9e388c0c190df92f933d1aa65532cd86ae89e2c0
Tom Kristian Tjemsland <tom.tjemsland@bouvet.no>
2019-11-06 15:15:26 +0100

    Updated rollup for typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,51 @@
 import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+      },
+    ],
+    external: [
+      ...Object.keys(pkg.dependencies),
+    ],
+    plugins: [
+      typescript({
+        // eslint-disable-next-line global-require
+        typescript: require('typescript'),
+      }),
+      terser({
+        mangle: false,
+      }),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'Vector2',
+      file: pkg.browser,
+      format: 'umd',
     },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-
-  plugins: [
-    typescript({
-      // eslint-disable-next-line global-require
-      typescript: require('typescript'),
-    }),
-    terser({
-      mangle: false,
-    }),
-  ],
-};
+    plugins: [
+      resolve(),
+      typescript({
+        // eslint-disable-next-line global-require
+        typescript: require('typescript'),
+      }),
+      terser({
+        mangle: false,
+      }),
+    ],
+  },
+];
