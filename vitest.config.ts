a1b4fbe1f21ddff15b9feffcdbf0fd3bf21b819d
Svein Erik Lundervik Fornes <41449429+sefornes@users.noreply.github.com>
2026-08-31 10:19:44 +0200

    ci: add workflow for code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,16 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['test/**/*.test.ts'],
+    reporter: ['default', 'junit'],
+    outputFile: {
+      junit: 'test-results/junit.xml',
+    },
+    coverage: {
+      provider: 'v8',
+      all: true,
+      include: ['src/**/*.ts'],
+      reporter: [['cobertura', { file: 'Cobertura.xml' }]],
+      reportsDirectory: 'coverage',
+    },
   },
 });
